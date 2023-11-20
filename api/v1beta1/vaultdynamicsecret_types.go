84c5a787e0a4c9cae6546090b04eb8c44cd2d8df
Adrian Moisey <adrian@changeover.za.net>
2023-11-20 09:55:01 +0200

    Add a configurable delay before writing a dynamic secret
    Fixes https://github.com/hashicorp/vault-secrets-operator/issues/271
    
    Some services (such as AWS IAM) are eventually consistent and require
    some time between generating the secret, and using the secret.
    
    Without this delay our services can't access AWS for a short while
    immediately after the secret rotation happens.

--- api/v1beta1/vaultdynamicsecret_types.go
+++ api/v1beta1/vaultdynamicsecret_types.go
@@ -60,6 +60,11 @@ type VaultDynamicSecretSpec struct {
 	RolloutRestartTargets []RolloutRestartTarget `json:"rolloutRestartTargets,omitempty"`
 	// Destination provides configuration necessary for syncing the Vault secret to Kubernetes.
 	Destination Destination `json:"destination"`
+	// Delay adds an artifical delay between fetching the secret and writing it to Kubernetes.
+	// This is to cater for services that are eventually consistent
+	// +kubebuilder:default=0
+	// +kubebuilder:validation:Minimum=0
+	Delay int `json:"delay,omitempty"`
 	// RefreshAfter a period of time for VSO to sync the source secret data, in
 	// duration notation e.g. 30s, 1m, 24h. This value only needs to be set when
 	// syncing from a secret's engine that does not provide a lease TTL in its
